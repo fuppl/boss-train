fabe05255f91f48800f99a6a1422d61a1f229d27
fuppl <2766700872@qq.com>
2020-07-14 14:58:04 +0800

    Develop1.java 1

--- src/Develop1.java
+++ src/Develop1.java
@@ -1,5 +1,7 @@
 public class Develop1 {
     public static void main(String[] args) {
         System.out.println("branch develop/gitUser1 init");
+        System.out.println("branch develop/gitUser1-2 , it has become different");
+
     }
 }
