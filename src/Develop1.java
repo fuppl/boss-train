87337244c340ab8f4ed09e860f75380a18651e60
dawnight1990 <j.y.li1@nefu.edu.cn>
2020-07-14 14:58:49 +0800

    dawnight1990 Develop1.java 1

--- src/Develop1.java
+++ src/Develop1.java
@@ -1,5 +1,6 @@
 public class Develop1 {
     public static void main(String[] args) {
         System.out.println("branch develop/gitUser1 init");
+        System.out.println("Here is dawnight1990");
     }
 }
