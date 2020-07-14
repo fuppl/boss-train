78cbdaa7bc1a2f5e9a09721f70f1ed47c149c5a7
dawnight1990 <j.y.li1@nefu.edu.cn>
2020-07-14 14:04:53 +0800

    git-user2创建类3

--- src/Dawnight.java
+++ src/Dawnight.java
@@ -1,6 +1,5 @@
 public class Dawnight {
     public static void main(String[] args) {
-        System.out.println("This is Dawnight1990");
-        System.out.println("hahaha");
+        System.out.println("模拟冲突");
     }
 }
