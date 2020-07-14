b1ae51caa15ef2348ecd42509062abd02cce6fca
fuppl <2766700872@qq.com>
2020-07-14 14:01:40 +0800

    fuppl创建类2

--- src/Dawnight.java
+++ src/Dawnight.java
@@ -1,6 +1,6 @@
 public class Dawnight {
     public static void main(String[] args) {
         System.out.println("This is Dawnight1990");
-        System.out.println("lalala");
+        System.out.println("hahaha");
     }
 }
